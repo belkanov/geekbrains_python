2f6fdf2362a069ea0d563ec238b9d2e838b94183
belkanov <78809180+belkanov@users.noreply.github.com>
2021-10-13 21:34:13 +0500

    ДЗ Урок 2. Шаблон + Контекст = html

--- SECOND_3/DJANGO_1/geekshop/mainapp/views.py
+++ SECOND_3/DJANGO_1/geekshop/mainapp/views.py
@@ -3,4 +3,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def products(requset):
-    return render(requset, 'mainapp/products.html')
+    context = {
+        'title': 'каталог',
+        'menu_links': [
+            {'href': 'main', 'name': 'домой'},
+            {'href': 'products', 'name': 'продукты'},
+            {'href': 'contacts', 'name': 'контакты'},
+        ]
+    }
+    return render(requset, 'mainapp/products.html', context)

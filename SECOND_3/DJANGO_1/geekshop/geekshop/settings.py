2a832941e916b3156b8d79deddb1902d678db26b
belkanov <78809180+belkanov@users.noreply.github.com>
2021-11-26 19:50:36 +0500

    ДЗ Урок 5. Развертывание Django-проекта на VPS от REG.RU

--- SECOND_3/DJANGO_1/geekshop/geekshop/settings.py
+++ SECOND_3/DJANGO_1/geekshop/geekshop/settings.py
@@ -27,9 +27,10 @@ environ.Env.read_env(str(BASE_DIR / '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['194.58.102.112']
 
 # Application definition
 
@@ -88,10 +89,16 @@ WSGI_APPLICATION = 'geekshop.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'geekshop',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'postgres',
     }
+
 }
 
 # Password validation

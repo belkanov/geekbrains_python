0532cbf78c971369afa2659956faa6a2e3c6ec9e
belkanov <78809180+belkanov@users.noreply.github.com>
2021-12-07 21:47:30 +0500

    ДЗ Урок 1. Введение в REST и Django REST Framework

--- SECOND_3/DRF/todolist/authapp/views.py
+++ SECOND_3/DRF/todolist/authapp/views.py
@@ -1,3 +1,12 @@
+from rest_framework.viewsets import ModelViewSet
+from .models import User
+from .serializers import UserModelSerializer
+
 from django.shortcuts import render
 
+
 # Create your views here.
+
+class UserModelViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserModelSerializer
--- SECOND_3/DRF/todolist/todolist/settings.py
+++ SECOND_3/DRF/todolist/todolist/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'authapp',
 ]
 
 MIDDLEWARE = [
@@ -84,6 +88,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = 'authapp.User'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',

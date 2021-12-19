9abea632fb78141d7e7b4b496bbb6206123f5f6d
belkanov <78809180+belkanov@users.noreply.github.com>
2021-12-19 14:40:14 +0500

    ДЗ Урок 4. Views. Filtering. Pagination

--- SECOND_3/DRF/todolist/authapp/views.py
+++ SECOND_3/DRF/todolist/authapp/views.py
@@ -1,4 +1,5 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin
 from .models import User
 from .serializers import UserModelSerializer
 
@@ -7,6 +8,14 @@ from django.shortcuts import render
 
 # Create your views here.
 
-class UserModelViewSet(ModelViewSet):
+class UserModelViewSet(ListModelMixin, RetrieveModelMixin, UpdateModelMixin, GenericViewSet):
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
+
+    # потесил в постмане:
+    # - GET ОК http://127.0.0.1:8000/api/users/
+    # - GET ОК http://127.0.0.1:8000/api/users/4/
+    # - PUT ОК, ругается, если не все поля
+    # - PATCH OK, с одним полем
+    # - POST not allowed
+    # - DEL not allowed

73772b1c392f63a3041417f648ccb1f7cdcf1d7d
belkanov <78809180+belkanov@users.noreply.github.com>
2022-01-29 20:19:25 +0500

    ДЗ Урок 8. Тестирование API. Фабрики данных

--- SECOND_3/DRF/todolist/authapp/tests.py
+++ SECOND_3/DRF/todolist/authapp/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate
+from .views import UserModelViewSet
+from .models import User
 
-# Create your tests here.
+
+class TestUserViewSet(TestCase):
+
+    def test_get_users_not_auth(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/users/')
+        view = UserModelViewSet.as_view({'get': 'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)  # AnonReadOnly выключен
+
+    def test_get_users_auth(self):
+        factory = APIRequestFactory()
+        request = factory.get('/api/users/')
+        view = UserModelViewSet.as_view({'get': 'list'})
+        admin = User.objects.create_superuser('admin', 'admin@admin.com', '123')
+        force_authenticate(request, admin)
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)

530f2dbe8f6bcb53df10cae84ee7f296e71a2f63
belkanov <78809180+belkanov@users.noreply.github.com>
2022-03-20 21:28:54 +0500

    ДЗ Урок 8. Потоки (init)

---
+++ THIRD_1/lesson_8/practice/log_config/server.py
@@ -0,0 +1,23 @@
+import logging
+from logging import handlers
+
+import base
+
+log = logging.getLogger('app.server')
+log.setLevel(logging.DEBUG)
+
+file_handler = logging.handlers.TimedRotatingFileHandler(base.LOG_DIR / 'server.log', when='midnight', encoding='utf-8')
+file_handler.setFormatter(base.FORMATTER)
+log.addHandler(file_handler)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(base.FORMATTER)
+log.addHandler(stream_handler)
+
+if __name__ == '__main__':
+    log.critical('test critical log msg')
+    log.error('test error log msg')
+    log.warning('test warning log msg')
+    log.info('test info log msg')
+    log.debug('test debug log msg')
+

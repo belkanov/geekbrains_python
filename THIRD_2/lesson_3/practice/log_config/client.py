3032df3dfb9a63f8859d5affb1f061140d5a599a
belkanov <78809180+belkanov@users.noreply.github.com>
2022-04-07 22:36:53 +0500

    ДЗ Урок 3. Хранение данных в БД. ORM SQLAlchemy (init)

---
+++ THIRD_2/lesson_3/practice/log_config/client.py
@@ -0,0 +1,21 @@
+import logging
+
+from . import base
+
+log = logging.getLogger('app.client')
+log.setLevel(logging.ERROR)
+
+file_handler = logging.FileHandler(base.LOG_DIR / 'client.log', encoding='utf-8')
+file_handler.setFormatter(base.FORMATTER)
+log.addHandler(file_handler)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(base.FORMATTER)
+log.addHandler(stream_handler)
+
+if __name__ == '__main__':
+    log.critical('test critical log msg')
+    log.error('test error log msg')
+    log.warning('test warning log msg')
+    log.info('test info log msg')
+    log.debug('test debug log msg')

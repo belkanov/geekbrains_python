742583a4cd1ef82c4c7ee0ae6a8b967513466c5e
belkanov <78809180+belkanov@users.noreply.github.com>
2022-05-30 22:24:01 +0500

    ДЗ Урок 5. Selenium в Python

--- OPTIONAL/PARSING/lesson_5/constants.py
+++ OPTIONAL/PARSING/lesson_5/constants.py
@@ -10,12 +10,9 @@ MONGODB_DB_NAME = os.environ.get('MONGODB_DB_NAME')
 MONGO_INITDB_ROOT_USERNAME = os.environ.get('MONGO_INITDB_ROOT_USERNAME')
 MONGO_INITDB_ROOT_PASSWORD = os.environ.get('MONGO_INITDB_ROOT_PASSWORD')
 
-MONGO_COLLECTION = 'mailru_main_news'
+MONGO_COLLECTION = os.environ.get('MONGO_COLLECTION')
 
-MAIN_URL = 'https://news.mail.ru/'
-XPATH_ANCHOR = '//table[@class="daynews__inner"]'
-HEADERS = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.67 Safari/537.36'
-}
-PARAMS = {
-}
+EMAIL_USERNAME = os.environ.get('EMAIL_USERNAME')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+
+MAIN_URL = os.environ.get('MAIN_URL', 'https://account.mail.ru/login')
--- OPTIONAL/PARSING/lesson_5/lesson_5.py
+++ OPTIONAL/PARSING/lesson_5/lesson_5.py
@@ -1,25 +1,28 @@
 """
-1. Развернуть у себя на компьютере/виртуальной машине/хостинге MongoDB и
-реализовать функцию, которая будет добавлять только новые вакансии в вашу базу.
+Вариант I
+Написать программу, которая собирает входящие письма из своего или тестового
+почтового ящика и сложить данные о письмах в базу данных
+(от кого, дата отправки, тема письма, текст письма полный)
 
-2. Написать функцию, которая производит поиск и выводит на экран вакансии с
-заработной платой больше введённой суммы (необходимо анализировать оба поля зарплаты).
-Для тех, кто выполнил задание с Росконтролем - напишите запрос для поиска продуктов
-с рейтингом не ниже введенного или качеством не ниже введенного
-(то есть цифра вводится одна, а запрос проверяет оба поля)
+Вариант II
+2) Написать программу, которая собирает товары «В тренде» с сайта техники
+mvideo и складывает данные в БД. Сайт можно выбрать и свой.
+Главный критерий выбора: динамически загружаемые товары
 """
 import logging
 from hashlib import sha3_256
-from pprint import pprint
 from time import sleep
 from typing import Optional
-from lxml import html
 
 import requests
 from pymongo import MongoClient
 from pymongo.collection import Collection
 from pymongo.errors import DuplicateKeyError
-from bson.objectid import ObjectId
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 from constants import *
 
@@ -28,91 +31,100 @@ logging.basicConfig(format='%(asctime)s | %(levelname)-8s | %(name)s | %(message
 logger = logging.getLogger('job_scraper')
 logger.setLevel(logging.INFO)
 
-# если включить - можно увидеть редиректы
-# requests_log = logging.getLogger("urllib3")
-# requests_log.setLevel(logging.DEBUG)
-# requests_log.propagate = True
 
+def parse_mails(url):
+    service = Service(executable_path='./chromedriver_win32_102.0.5005.61.exe')
+    options = Options()
+    options.add_argument('start-maximized')
 
-def get_response(url, headers=HEADERS, params=None):
-    timeouts = (5, 5)  # conn, read
+    with webdriver.Chrome(service=service, options=options) as driver:
+        driver.implicitly_wait(10)
 
-    for i in range(5):
-        response = requests.get(url,
-                                headers=headers,
-                                params=params,
-                                timeout=timeouts)
-        if response.ok:
-            logger.debug('response - OK')
-            break
+        mails_data = []
+        # на простой странице (https://mail.ru) у меня не находил тэги
+        # для ввода логопаса.
+        # Искал через xpath, id, имена тэгов. Даже самым широким поиском
+        # в списке не было искомых. Не понял в чем печаль.
+        # Возможно плохо дружит с iframe - хз..
+        driver.get(url)
+
+        username_field = driver.find_element(By.CSS_SELECTOR, 'input[name="username"]')
+        username_field.send_keys('study.ai_172@mail.ru')
+        username_field.send_keys(Keys.ENTER)
+        password_field = driver.find_element(By.CSS_SELECTOR, 'input[name="password"]')
+        password_field.send_keys('NextPassword172#')
+        password_field.send_keys(Keys.ENTER)
+
+        # Решил немного поиграться с интерфейсом.
+        # Возможно так менее эффективно, чем просто собрать ссылки,
+        # а потом по ним пройтись, но так явно интересней =)
+        #
+        # ЭТО ЛОМАЕТ ОТОБРАЖЕНИЕ ПИСЕМ ДЛЯ ОСТАЛЬНЫХ УЧАСТНИКОВ
+        # поэтому если проверять, то где-нить на отдельном ящике,
+        # либо после тестов - вернуть галку обратно
+
+        # кнопка "настройки" слева внизу
+        driver.find_element(By.CSS_SELECTOR, 'div.settings').click()
+        # галка "Список писем с колонкой письма"
+        if driver.find_elements(By.CSS_SELECTOR, 'div[data-test-id="3pane-enabled"]'):
+            pass
         else:
-            logger.debug('response - NOT OK (%s)', response.status_code)
-            sleep_time = i + 1
-            logger.warning(f'Не смог получить ответ для %s. Подождем %d сек.',
-                           response.url,
-                           sleep_time)
-            sleep(sleep_time)
-    else:
-        raise SystemExit(1, f'Так и не смог получить ответ для {response.url}')
+            driver.find_element(By.CSS_SELECTOR, 'div[data-test-id="3pane-disabled"]').click()
+        # крестик "Закрыть настройки"
+        driver.find_element(By.CSS_SELECTOR, 'button[data-test-id="cross"]').click()
 
-    return response
+        # Искусственное ограничение, чтобы не собирать все 1к+ писем.
+        # По-хорошему, если делать это на постоянке, то нас будут интересовать
+        # только непрочитанные, которые тоже можно найти.
+        max_mails_cnt = 10
+        mails_cnt = 0
+        prev_mail_link = ''
 
+        # находим первое письмо и открываем его
+        driver.find_element(By.CSS_SELECTOR, 'a.llct').click()
+        while True:
+            # там же есть еще llct_active, но я решил по llct_open.
+            # хз куда актив может указать, а llct_open, по логике должен
+            # смотреть на открытое, а оно одно в данной ситуации
+            current_mail = driver.find_element(By.CSS_SELECTOR, 'a.llct_open')
+            current_mail_link = current_mail.get_attribute('href')
+            if prev_mail_link == current_mail_link or mails_cnt >= max_mails_cnt:
+                break
 
-def save_to_file(data, file_name):
-    with open(file_name, 'w', encoding='utf8') as f:
-        f.write(data)
+            prev_mail_link = current_mail_link
+            mails_cnt += 1
 
+            # тема письма, она тут полная.
+            # Обрезается видимо JSом или вообще стилями, я хз..
+            # Где-то 1 из 5 - не собиралась тема первого письма.
+            # Я не стал ковырять - думаю затупы моего ПК.
+            mail_title = current_mail.find_element(By.CSS_SELECTOR, 'div.llct__subject').get_attribute('title')
+            # от кого (название+ящик)
+            mail_from = current_mail.find_element(By.CSS_SELECTOR, 'span.ll-crpt').get_attribute('title')
+            # дата отправки. Надо бы еще переделать "Сегодня/Вчера/ХХ мая/..." на
+            # адекватные значения, но я не стал - все таки не боевая задача =)
+            mail_send_date = current_mail.find_element(By.CSS_SELECTOR, 'div.llct__date').get_attribute('title')
+            # текст письма
+            # выше в DOM есть "letter__body", но в нем еще какие-то календари и обертки
+            # решил их не брать
+            mail_body_text = driver.find_element(By.CSS_SELECTOR, 'div.letter-body__body-content').text
 
-def is_link_ok(link):
-    # Натыкался на разное, что, по идее, не подходит под стандартные новости.
-    # Например
-    # https://news.mail.ru/diafilm/12-po-rossii-kazan/
-    # слайдшоу по достопримечательностям Казани
-    #
-    # https://news.mail.ru/stories/4-foto-dnya/
-    # там галерея из 35 фото с изначально пустыми ДИВами,
-    # Дата самой "новости" всегда 3 ноября 2021 (поэтому ее не беру),
-    # актуальные даты есть только у фото, которые заполняются динамически.
-    #
-    # решил такое пропускать
-    if link.split('/')[-2].isdecimal():
-        return True
-    logger.warning('ПРОПУЩЕНО: скорее всего не обычная новость: %s', link)
-    return False
+            # переходим к следующему письму
+            current_mail.send_keys(Keys.ARROW_DOWN)
 
+            mails_data.append({
+                'title': mail_title,
+                'from': mail_from,
+                'send_date': mail_send_date,
+                'body_text': mail_body_text,
+            })
 
-def parse_response(response):
-    news_info_list = []
-    news_links = []
-    dom = html.fromstring(response.text)
-    anchor = dom.xpath(XPATH_ANCHOR)[0]
+            # попробуем сразу двух зайцев
+            # 1 - будем косить под пользователя и не жать на кнопку 500раз/сек
+            # 2 - дадим тексту письма загрузиться (хоть это и не самый кошерный вариант)
+            sleep(2)
 
-    # сначала картинки (5шт)
-    news_links.extend(anchor.xpath('.//a/@href'))
-    # потом текст под ними (6шт)
-    news_links.extend(anchor.xpath('../following-sibling::ul/li[not(contains(@class, "hidden"))]/a/@href'))
-
-    logger.info('Нашел %d новостей. Обрабатываю...', len(news_links))
-    for news_idx, news_link in enumerate(news_links):
-        logger.info('Parse news #%d', news_idx+1)
-        if not is_link_ok(news_link):
-            continue
-
-        news_response = get_response(news_link)
-        news_dom = html.fromstring(news_response.text)
-        news_anchor = news_dom.xpath('//div[@data-logger-parent="content"]')[0]
-
-        metainfo_anchor = news_anchor.xpath('.//span[@class="note"]')
-        news_info = {
-            'link': str(news_link),  # приводим к явной строке <_ElementUnicodeResult>
-            'pub_date': metainfo_anchor[0].xpath('./span/@datetime')[0],
-            'source_name': metainfo_anchor[1].xpath('./a//text()')[0],
-            'source_link': metainfo_anchor[1].xpath('./a/@href')[0],  # решил добавить
-            'news_title': news_anchor.xpath('.//h1/text()')[0],
-        }
-        news_info_list.append(news_info)
-
-    return news_info_list
+    return mails_data
 
 
 def get_mongo_collection(collection_name: str) -> Optional[Collection]:
@@ -130,17 +142,17 @@ def get_mongo_collection(collection_name: str) -> Optional[Collection]:
 
 
 def save_to_mongo(data):
-    mailru_news_collection = get_mongo_collection(MONGO_COLLECTION)
+    mails_collection = get_mongo_collection(MONGO_COLLECTION)
     for row in data:
         try:
-            # если вдруг будет http вместо https или поменяется поддомен, то
-            # получим другой хэш (даже без него все равно уникальность сломается).
-            # Хочется чуть больше стабильности =)
-            # 'https://news.mail.ru/society/51496205/' =>
-            # 'society51496205'
-            news_unique_str = ''.join(row['link'].split('/')[3:5])
-            news_hash = sha3_256(news_unique_str.encode('utf-8')).hexdigest()
-            mailru_news_collection.insert_one({'_id': news_hash, **row})
+            # При условии преобразования даты в нормальное значение
+            # (о чем я писал выше и не делал) - можно получить норм хэш для одного
+            # и того же письма, которое мы соберем сегодня, а потом завтра.
+            # Можно конечно и ИД брать, который есть в URL или
+            # data-id="0:16539077630958936900:0"
+            mail_unique_str = ''.join((row['from'], row['send_date']))
+            mail_hash = sha3_256(mail_unique_str.encode('utf-8')).hexdigest()
+            mails_collection.insert_one({'_id': mail_hash, **row})
         except DuplicateKeyError as e:
             logger.debug('Для ID=%s уже есть запись. Пропускаем', vacancy_id)  # noqa
             pass
@@ -148,21 +160,16 @@ def save_to_mongo(data):
 
 def main():
     url = MAIN_URL
-    logger.info('Parse %s', url)
-    response = get_response(url)
-    if not response:
-        logger.error('NO response from %s', url)
-        raise SystemExit(1)
+    logger.info('Parse mails...')
 
     try:
-        news_info_list = parse_response(response)
-    except ValueError as e:
+        mails_list = parse_mails(url)
+    except Exception as e:
         logger.exception(e)
-        save_to_file(response.text, 'error_response.html')
         raise SystemExit(1)
 
     logger.info('Сохраняю их в Mongo')
-    save_to_mongo(news_info_list)
+    save_to_mongo(mails_list)
 
 
 if __name__ == '__main__':
